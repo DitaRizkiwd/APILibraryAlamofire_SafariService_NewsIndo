80fb57b312d84bd39036286bdc321727d7742bc2
DitaRizkiwd <rdita201@gmail.com>
2024-04-23 09:54:50 +0700

    API Service fetch news using alamofire library

---
+++ NewsIndo/Services/APIService.swift
@@ -0,0 +1,34 @@
+//
+//  APIService.swift
+//  NewsIndo
+//
+//  Created by MACBOOK PRO on 23/04/24.
+//
+
+import Foundation
+import Alamofire
+
+class APIService{
+    static let shared = APIService()
+    
+    private init(){}
+    
+    func fetchNews() async throws -> [NewsArticle]{
+        guard let url = URL(string: Constant.newsUrl) 
+        else {
+            throw URLError(.badURL)
+        }
+        //set data, response data, handle error bawaan sistem
+        let news = try await withCheckedThrowingContinuation { continuation in
+            AF.request(url).responseDecodable(of: NewsModel.self) { response in
+                switch response.result{
+                case .success(let newsResponse):
+                    continuation.resume(returning: newsResponse.data)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+        return news
+    }
+}
---
+++ NewsIndo/Services/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  NewsIndo
+//
+//  Created by MACBOOK PRO on 23/04/24.
+//
+
+import Foundation
+struct Constant {
+    static let newsUrl = "https://berita-indo-api-next.vercel.app/api/kumparan-news/"
+    
+    static let newsTitle = "Berita Indo"
+}
